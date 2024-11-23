6d9b908cad71c93d30ea7f3fae6e77365452a5fb
sergey162 <144516112+sergey162@users.noreply.github.com>
2024-11-23 14:01:17 +0300

    Create simple_numbers.hpp

---
+++ labs/lab4/simple_numbers/simple_numbers.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+
+extern "C" {
+
+std::int32_t FastCountSimpleNumbers(std::int32_t, std::int32_t);
+
+std::int32_t BadCountSimpleNumbers(std::int32_t, std::int32_t);
+
+}
